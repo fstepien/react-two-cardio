e8e7fa9a7256f3849ae2536d242909360e9c30b3
Filip Stepien <contact@filipstepien.com>
2018-05-26 10:12:01 -0400

    add toggle, modal, portal

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { Component, Fragment } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Toggle from "./ToggleRPC";
+import Portal from "./Portal";
+import Modal from "./Modal";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+
+        <Toggle>
+          {({ on, toggle }) => (
+            <Fragment>
+              <button onClick={toggle}>Email My Results</button>
+              <Modal on={on} toggle={toggle}>
+                <h1>Email My Results</h1>
+              </Modal>
+            </Fragment>
+          )}
+        </Toggle>
+      </div>
+    );
+  }
+}
+
+export default App;
